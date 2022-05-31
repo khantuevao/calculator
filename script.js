42d12fc515319791baac01f3c85daf0637e972bf
khantuevao <khantuevao@gmail.com>
2022-05-31 19:46:14 +0200

    Make delete button do its work even in result

--- script.js
+++ script.js
@@ -32,8 +32,142 @@ function operate(operator, a, b) {
     }
 } 
 
+let checkDot = () => {
+    let checkArray = (lowerscreen.textContent).split('');
+    let findArray = checkArray.includes('.');
+    return findArray;
+}
+
+const dot = document.querySelector('#dot');
+dot.addEventListener('click', () => {
+    if (checkDot()) {
+        return;
+    } else {
+        lowerscreen.textContent += '.';
+        displayValue = lowerscreen.textContent;
+    }
+});
+
 const lowerscreen = document.querySelector('#lowerscreen');
 const upperscreen = document.querySelector('#upperscreen');
+
+let storeValue = 0;
+let displayValue = 0;
+let operator;
+let result;
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', clearValue);
+
+function clearValue() {
+    lowerscreen.textContent = '0';
+    upperscreen.textContent = '';
+    displayValue = 0;
+    storeValue = 0;
+}
+
+const backspace = document.querySelector('#backspace');
+backspace.addEventListener('click', () => {
+        lowerscreen.textContent = (lowerscreen.textContent).slice(0, -1);
+        displayValue = lowerscreen.textContent;
+        result = displayValue;
+})
+
+const plus = document.querySelector('#plus');
+plus.addEventListener('click', () => {
+    if (storeValue === 0 || displayValue === result) {
+        storeValue = displayValue;
+        upperscreen.textContent = `${storeValue} +`;
+        displayValue = 0;
+    } else {
+        equalBt();
+        upperscreen.textContent = `${storeValue} +`;
+        displayValue = 0;
+    }
+    operator = 'add';
+})
+
+const minus = document.querySelector('#minus');
+minus.addEventListener('click', () => {
+    if (storeValue === 0 || displayValue === result) {
+        storeValue = displayValue;
+        upperscreen.textContent = `${storeValue} -`;
+        displayValue = 0;
+    } else {
+        equalBt();
+        upperscreen.textContent = `${storeValue} -`;
+        displayValue = 0;
+    }
+    operator = 'subtract';
+})
+
+const multiplyBtn = document.querySelector('#multiplyBtn');
+multiplyBtn.addEventListener('click', () => {
+    if (storeValue === 0 || displayValue === result) {
+        storeValue = displayValue;
+        upperscreen.textContent = `${storeValue} ×`;
+        displayValue = 0;
+    } else {
+        equalBt();
+        upperscreen.textContent = `${storeValue} ×`;
+        displayValue = 0;
+    }
+    operator = 'multiply';
+})
+
+const divideBtn = document.querySelector('#divideBtn');
+divideBtn.addEventListener('click', () => {
+    if (storeValue === 0 || displayValue === result) {
+        storeValue = displayValue;
+        upperscreen.textContent = `${storeValue} ÷`;
+        displayValue = 0;
+    } else {
+        equalBt();
+        upperscreen.textContent = `${storeValue} ÷`;
+        displayValue = 0;
+    }
+    operator = 'divide';
+})
+
+const equal = document.querySelector('#equal');
+equal.addEventListener('click', equalBt);
+
+function equalBt() {
+    if (operator === 'add') {
+        result = +(add(storeValue, displayValue)).toFixed(2);
+        upperscreen.textContent = `${storeValue} + ${displayValue} =`;
+        displayValue = result;
+        lowerscreen.textContent = result
+        storeValue = result;
+        operator = '';
+    } else if (operator === 'subtract') {
+        result = +(subtract(storeValue, displayValue)).toFixed(2);
+        upperscreen.textContent = `${storeValue} - ${displayValue} =`;
+        displayValue = result;
+        lowerscreen.textContent = result;
+        storeValue = result;
+        operator = '';
+    } else if (operator === 'multiply') {
+        result = +(multiply(storeValue, displayValue)).toFixed(2);
+        upperscreen.textContent = `${storeValue} × ${displayValue} =`;
+        displayValue = result;
+        lowerscreen.textContent = result
+        storeValue = result;
+        operator = '';
+    } else if (operator === 'divide'){
+        result = +(divide(storeValue, displayValue)).toFixed(2);
+        if (result === undefined) {
+            clearValue()
+            result = 0;
+        } else {
+            upperscreen.textContent = `${storeValue} ÷ ${displayValue} =`;
+            displayValue = result;
+            lowerscreen.textContent = result
+            storeValue = result;
+            operator = '';
+        }
+    }
+}
 
 const one = document.querySelector('#one');
 one.addEventListener('click', () => {
@@ -135,146 +269,3 @@ zero.addEventListener('click', () => {
     }
     displayValue = lowerscreen.textContent;
 });
-
-let checkDot = () => {
-    let checkArray = (lowerscreen.textContent).split('');
-    let findArray = checkArray.includes('.');
-    return findArray;
-}
-
-const dot = document.querySelector('#dot');
-dot.addEventListener('click', () => {
-    if (checkDot()) {
-        return;
-    } else {
-        lowerscreen.textContent += '.';
-        displayValue = lowerscreen.textContent;
-    }
-});
-
-function clearValue() {
-    lowerscreen.textContent = '0';
-    upperscreen.textContent = '';
-    displayValue = 0;
-    storeValue = 0;
-}
-
-const clear = document.querySelector('#clear');
-clear.addEventListener('click', clearValue)
-
-const backspace = document.querySelector('#backspace');
-backspace.addEventListener('click', () => {
-    if ((lowerscreen.textContent).length === 0) {
-        lowerscreen.textContent = '0';
-    } else if (displayValue === result) {
-        clearValue()
-        lowerscreen.textContent = '0';
-    } else {
-        lowerscreen.textContent = (lowerscreen.textContent).slice(0, -1);
-        displayValue = lowerscreen.textContent;
-    }
-})
-
-let storeValue = 0;
-let displayValue = 0;
-let oldDisplayValue = 0;
-let oldStoreValue = 0;
-let operator;
-let result;
-
-const plus = document.querySelector('#plus');
-plus.addEventListener('click', () => {
-    if (storeValue === 0 || displayValue === result) {
-        storeValue = displayValue;
-        upperscreen.textContent = `${storeValue} +`;
-        displayValue = 0;
-    } else {
-        equalBt();
-        upperscreen.textContent = `${storeValue} +`;
-        displayValue = 0;
-    }
-    operator = 'add';
-})
-
-const minus = document.querySelector('#minus');
-minus.addEventListener('click', () => {
-    if (storeValue === 0 || displayValue === result) {
-        storeValue = displayValue;
-        upperscreen.textContent = `${storeValue} -`;
-        displayValue = 0;
-    } else {
-        equalBt();
-        upperscreen.textContent = `${storeValue} -`;
-        displayValue = 0;
-    }
-    operator = 'subtract';
-})
-
-const multiplyBtn = document.querySelector('#multiplyBtn');
-multiplyBtn.addEventListener('click', () => {
-    if (storeValue === 0 || displayValue === result) {
-        storeValue = displayValue;
-        upperscreen.textContent = `${storeValue} ×`;
-        displayValue = 0;
-    } else {
-        equalBt();
-        upperscreen.textContent = `${storeValue} ×`;
-        displayValue = 0;
-    }
-    operator = 'multiply';
-})
-
-const divideBtn = document.querySelector('#divideBtn');
-divideBtn.addEventListener('click', () => {
-    if (storeValue === 0 || displayValue === result) {
-        storeValue = displayValue;
-        upperscreen.textContent = `${storeValue} ÷`;
-        displayValue = 0;
-    } else {
-        equalBt();
-        upperscreen.textContent = `${storeValue} ÷`;
-        displayValue = 0;
-    }
-    operator = 'divide';
-})
-
-const equal = document.querySelector('#equal');
-equal.addEventListener('click', equalBt);
-
-function equalBt() {
-    if (operator === 'add') {
-        result = +(add(storeValue, displayValue)).toFixed(2);
-        upperscreen.textContent = `${storeValue} + ${displayValue} =`;
-        displayValue = result;
-        lowerscreen.textContent = result
-        storeValue = result;
-        operator = '';
-    } else if (operator === 'subtract') {
-        result = +(subtract(storeValue, displayValue)).toFixed(2);
-        upperscreen.textContent = `${storeValue} - ${displayValue} =`;
-        displayValue = result;
-        lowerscreen.textContent = result;
-        storeValue = result;
-        operator = '';
-    } else if (operator === 'multiply') {
-        result = +(multiply(storeValue, displayValue)).toFixed(2);
-        upperscreen.textContent = `${storeValue} × ${displayValue} =`;
-        displayValue = result;
-        lowerscreen.textContent = result
-        storeValue = result;
-        operator = '';
-    } else if (operator === 'divide'){
-        result = +(divide(storeValue, displayValue)).toFixed(2);
-        if (result === undefined) {
-            clearValue()
-            result = 0;
-        } else {
-            upperscreen.textContent = `${storeValue} ÷ ${displayValue} =`;
-            displayValue = result;
-            lowerscreen.textContent = result
-            storeValue = result;
-            operator = '';
-        }
-    }
-}
-
