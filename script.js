67e384663dec45459ef6970662d28efd594dee86
khantuevao <khantuevao@gmail.com>
2023-03-18 10:50:54 +0600

    Create calculator structure in html & style it. Create functions to populate display with numbers on click

--- script.js
+++ script.js
@@ -13,3 +13,35 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b
 }
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case '+':
+            return add(a, b)
+            break
+        case '-':
+            return subtract(a, b)
+            break
+        case '*':
+            return multiply(a, b)
+            break
+        case '/':
+            return divide(a, b)
+            break
+    }
+}
+
+let displayValue = '0'
+
+const numbers = document.querySelectorAll('.number')
+numbers.forEach(number => {
+    number.addEventListener('click', () => {
+        displayValue += number.textContent
+        showDisplayValue()
+    })
+})
+
+function showDisplayValue() {
+    const display = document.querySelector('.display')
+    display.textContent = Number(displayValue)
+}
