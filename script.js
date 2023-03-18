b90c2c4c959d3fa30829704feaa1622cd4ce16eb
khantuevao <khantuevao@gmail.com>
2023-03-18 14:23:58 +0600

    Make calc work

--- script.js
+++ script.js
@@ -32,12 +32,22 @@ function operate(operator, a, b) {
 }
 
 let displayValue = '0'
+let temp;
+let sign;
+
 
 const numbers = document.querySelectorAll('.number')
 numbers.forEach(number => {
     number.addEventListener('click', () => {
-        displayValue += number.textContent
-        showDisplayValue()
+        if ((!!temp)) {
+            displayValue = 0
+            displayValue += number.textContent
+            showDisplayValue()
+        } else {
+            displayValue += number.textContent
+            showDisplayValue()
+        }
+        
     })
 })
 
@@ -45,3 +55,58 @@ function showDisplayValue() {
     const display = document.querySelector('.display')
     display.textContent = Number(displayValue)
 }
+
+const clear = document.querySelector('.clear')
+clear.addEventListener('click', clearValue)
+function clearValue() {
+    resetValues()
+    displayValue = '0'
+    showDisplayValue()
+}
+
+function resetValues() {
+    temp = undefined
+    sign = undefined
+}
+
+const operators = document.querySelectorAll('.operator')
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        sign = operator.textContent
+        //if 'other' & no temp - store temp & store sign & clear display
+        if ((sign === '+' || sign === '-' || sign === '*' || sign === '/') && !temp) {
+            temp = Number(displayValue)
+            displayValue = '0'
+            showDisplayValue()
+        }
+        //if 'other' & yes temp - calculate result & store in temp & clear display
+        else if ((sign === '+' || sign === '-' || sign === '*' || sign === '/') && !!temp) {
+            if ((sign === '/') && (Number(displayValue) === 0)) {
+                alert('Cannot divide by 0')
+                clearValue()
+            }
+            temp = operate(sign, temp, Number(displayValue))
+            displayValue = temp
+            showDisplayValue()
+        }
+    })
+})
+
+const equal = document.querySelector('.equal')
+equal.addEventListener('click', () => {
+    //if '=' & no temp & no sign - return
+    if (!temp) {
+        return
+    }
+    //if '=' & yes temp & yes sign - calculate the result & show on display
+    else {
+        if ((sign === '/') && (Number(displayValue) === 0)) {
+            alert('Cannot divide by 0')
+            clearValue()
+        }
+        const result = operate(sign, temp, Number(displayValue))
+        displayValue = result
+        showDisplayValue()
+        resetValues()
+    }
+})
